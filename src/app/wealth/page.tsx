12f2dd364878f454f45646669a8084017105b01c
Billy19191 <bill.ratchanon@gmail.com>
2025-02-24 01:16:40 +0700

    feat: add dayjs for date formatting use hooks for data fetching

--- src/app/wealth/page.tsx
+++ src/app/wealth/page.tsx
@@ -1,14 +1,42 @@
 'use client'
-import { queryHistoricalGraphQL } from '@/utils/queryHistoricalGraphQL'
-import { TypeAnimation } from 'react-type-animation'
-export default async function wealthPage() {
-  const morphoData = await queryHistoricalGraphQL({
-    startTimestamp: 1740236400,
-    endTimestamp: 1740307605,
-    interval: 'HOUR',
-  })
 
-  console.log(morphoData)
+import { queryHistoricalGraphQL } from '@/utils/queryHistoricalGraphQL'
+import { useEffect, useMemo, useRef, useState } from 'react'
+import { TypeAnimation } from 'react-type-animation'
+import dayjs from 'dayjs'
+export default function WealthPage() {
+  const [morphoData, setMorphoData] = useState()
+  const morphoDataRef = useRef(morphoData)
+  const memoizedMorphoData = useMemo(() => morphoData, [morphoData])
+
+  useEffect(() => {
+    queryHistoricalGraphQL({
+      startTimestamp: 1740236400,
+      endTimestamp: 1740330582,
+      interval: 'HOUR',
+    }).then((data) => {
+      setMorphoData(data)
+    })
+  }, [])
+
+  useEffect(() => {
+    if (morphoData) {
+      const cleanData =
+        memoizedMorphoData?.userByAddress?.vaultPositions[1].historicalState
+      cleanData.assets.map((asset) => {
+        console.log({
+          // x: new Date(asset.x * 1000).toLocaleString(),
+          x: dayjs.unix(asset.x).format('YYYY-MM-DD HH:mm:ss'),
+          y: asset.y.toFixed(2) / 1000000 + ' USDC',
+        })
+        return {
+          x: new Date(asset.x * 1000).toLocaleString(),
+          y: asset.y.toFixed(2) / 1000000,
+        }
+      })
+    }
+  }, [memoizedMorphoData, morphoData])
+
   return (
     <>
       <div className="w-screen h-screen flex items-center justify-center">
@@ -16,7 +44,7 @@ export default async function wealthPage() {
           sequence={[
             'Welcome to',
             1000,
-            `🤑 Billy's Wealth Dashboard`,
+            `🤑 Billy's House!`,
             2000,
             'Coming soon ...',
             5000,
