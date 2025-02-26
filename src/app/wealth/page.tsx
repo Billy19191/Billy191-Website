b42388edcc84d9c0fa6250388c7b7bc869a45069
Billy19191 <bill.ratchanon@gmail.com>
2025-02-27 04:02:11 +0700

    feat: optimize query with useMemo

--- src/app/wealth/page.tsx
+++ src/app/wealth/page.tsx
@@ -47,11 +47,16 @@ export default function WealthPage() {
   )
   const [isShowing, setIsShowing] = useState<boolean>(false)
 
-  const { data, loading, error } = useHistoricalGraphQL({
-    startTimestamp: dayjs().subtract(1, 'month').unix(),
-    endTimestamp: dayjs().unix(),
-    interval: 'HOUR',
-  })
+  const queryParams = useMemo(
+    () => ({
+      startTimestamp: dayjs().subtract(1, 'month').unix(),
+      endTimestamp: dayjs().unix(),
+      interval: 'HOUR',
+    }),
+    []
+  )
+
+  const { data, loading, error } = useHistoricalGraphQL(queryParams)
 
   useEffect(() => {
     if (!loading && !error && data) {
@@ -132,10 +137,10 @@ export default function WealthPage() {
   // console.log(newDataFormat)
   return (
     <div className="h-full">
-      <div className="text-center font-mono text-xl font-semibold mt-4 mb-12">{`Billy191's Balance`}</div>
+      <div className="text-center font-mono text-xl font-semibold mt-4 mb-6">{`Billy191's Balance`}</div>
 
       <div className="flex flex-col lg:flex-row font-mono w-full h-fit lg:h-full items-center justify-center gap-x-10">
-        <div className="lg:border rounded p-10 w-screen lg:w-5/12 h-72 lg:h-96">
+        <div className="lg:border rounded px-10 py-6 lg:p-10 w-screen lg:w-5/12 h-72 lg:h-96">
           {isShowing ? (
             <HistoricalBalanceChart
               newDataFormat={newDataFormat}
@@ -147,8 +152,8 @@ export default function WealthPage() {
             </div>
           )}
         </div>
-        <div className="w-screen lg:w-2/12 flex flex-col lg:gap-y-10 ">
-          <div className="lg:border rounded p-10 h-2/12">
+        <div className="w-screen lg:w-2/12 flex flex-col lg:gap-y-10">
+          <div className="lg:border rounded px-10 py-6 lg:p-10 h-2/12">
             {isShowing ? (
               <div className="text-center font-mono font-semibold text-lg">
                 <CurrentBalanceBox currentBalance={currentBalance} />
@@ -159,7 +164,7 @@ export default function WealthPage() {
               </div>
             )}
           </div>
-          <div className="lg:border rounded p-10 h-2/12">
+          <div className="lg:border rounded px-10 py-6 lg:p-10 h-2/12">
             {isShowing ? (
               <div className="text-center font-mono font-semibold text-lg">
                 <TodayPNLBox
